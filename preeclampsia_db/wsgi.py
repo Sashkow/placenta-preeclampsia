d60ae120a1f6e45397fd1040cda0662c4e1b357b
sashkow <lykhenko.olexandr@gmail.com>
2016-11-23 10:03:47 +0200

    commit from server side

--- preeclampsia_db/wsgi.py
+++ preeclampsia_db/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "preeclampsia_db.settings")
 
 application = get_wsgi_application()
+
+# def application(environ, start_response):
+#     if environ['mod_wsgi.process_group'] != '': 
+#         import signal
+#         os.kill(os.getpid(), signal.SIGINT)
+#     return [b"killed"]
+
+# def application(environ, start_response):
+#     status = '200 OK'
+#     output = b'Hello World!'
+
+#     response_headers = [('Content-type', 'text/plain'),
+#                         ('Content-Length', str(len(output)))]
+#     start_response(status, response_headers)
+
+#     return [output]
